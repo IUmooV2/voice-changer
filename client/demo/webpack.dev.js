8c02274033bda35d95e6168bbad8b54457acec8e
IUmooV2 <action528@gmail.com>
2026-08-13 13:40:04 -0400

    proxy MooVoice dev client to local engine

--- client/demo/webpack.dev.js
+++ client/demo/webpack.dev.js
@@ -2,6 +2,8 @@ const path = require("path");
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
 
+const engineTarget = process.env.MOOVOICE_ENGINE_URL || "http://127.0.0.1:18888";
+
 module.exports = merge(common, {
     mode: "development",
     devServer: {
@@ -16,6 +18,28 @@ module.exports = merge(common, {
         },
         host: "0.0.0.0",
         server: "https",
+        proxy: [
+            {
+                context: [
+                    "/info",
+                    "/performance",
+                    "/update_settings",
+                    "/upload_file",
+                    "/concat_uploaded_file",
+                    "/load_model",
+                    "/merge_model",
+                    "/update_model_default",
+                    "/onnx",
+                    "/onnx_export",
+                    "/model_type",
+                    "/socket.io",
+                ],
+                target: engineTarget,
+                secure: false,
+                changeOrigin: true,
+                ws: true,
+            },
+        ],
         watchFiles: {
             paths: ["src/**/*", "public/**/*"],
             options: { usePolling: false },
