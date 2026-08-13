c874eb1d70400faf9ff27019416d6eef179b3b9b
IUmooV2 <action528@gmail.com>
2026-08-13 13:11:34 -0400

    fix: update webpack dev server HTTPS configuration

--- client/demo/webpack.dev.js
+++ client/demo/webpack.dev.js
@@ -1,9 +1,9 @@
 const path = require("path");
-const { merge } = require('webpack-merge');
-const common = require('./webpack.common.js')
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common.js");
 
 module.exports = merge(common, {
-    mode: 'development',
+    mode: "development",
     devServer: {
         static: {
             directory: path.join(__dirname, "public"),
@@ -15,6 +15,6 @@ module.exports = merge(common, {
             },
         },
         host: "0.0.0.0",
-        https: true,
+        server: "https",
     },
-})
+});
